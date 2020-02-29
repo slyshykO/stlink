d423718e1885c839a5a8748d86d41cb30c016482
Oleksiy Slyshyk <alexslyshyk@gmail.com>
2020-02-29 16:15:45 +0200

    clean up .travis-lin-mingw.sh

--- .travis-lin-mingw.sh
+++ .travis-lin-mingw.sh
@@ -3,15 +3,12 @@
 DIR=$PWD
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-	p7zip --help
-
-	echo $PWD
-	echo "DIR=$DIR"
+	echo "WORK DIR:$DIR"
 	mkdir -p $DIR/build/linux-mingw32-release
 	cd $DIR/build/linux-mingw32-release
 	echo "cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./cmake/linux-mingw32.cmake -DCMAKE_INSTALL_PREFIX=$PWD/_install $DIR"
 	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./cmake/linux-mingw32.cmake -DCMAKE_INSTALL_PREFIX=$PWD/_install $DIR 
-	echo "cmake --build $DIR/build/linux-mingw32-release --target all"
+	echo "make"
 	make
 fi
 
