d5a29f4e649c17f58cf525b868e15a51b25c0b04
Oleksiy Slyshyk <alexslyshyk@gmail.com>
2020-02-29 12:06:58 +0200

    .travis-lin-mingw.sh: make build dir

--- .travis-lin-mingw.sh
+++ .travis-lin-mingw.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./cmake/linux-mingw32.cmake -S . -B ./build/linux-mingw32
-	cmake --build ./build/linux-mingw32 --target all
+	mkdir -p build/linux-mingw32-release
+	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./cmake/linux-mingw32.cmake -S . -B ./build/linux-mingw32-release
+	cmake --build ./build/linux-mingw32-release --target all
 fi
 
