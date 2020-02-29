881f37fd42f468693947678746ff7d208fbe2855
Oleksiy Slyshyk <alexslyshyk@gmail.com>
2020-02-29 14:32:45 +0200

    show $PWD in travis-lin-mingw

--- .travis-lin-mingw.sh
+++ .travis-lin-mingw.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+	echo $PWD
 	mkdir -p build/linux-mingw32-release
+	echo $PWD
 	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./cmake/linux-mingw32.cmake -S . -B ./build/linux-mingw32-release
 	cmake --build ./build/linux-mingw32-release --target all
 fi
