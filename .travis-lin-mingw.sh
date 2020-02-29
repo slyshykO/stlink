ae329eba015ec7ea0b3bc952d1a982f57924bce5
Oleksiy Slyshyk <alexslyshyk@gmail.com>
2020-02-29 15:38:34 +0200

    update .travis-lin-mingw.sh

--- .travis-lin-mingw.sh
+++ .travis-lin-mingw.sh
@@ -7,9 +7,9 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 	echo "DIR=$DIR"
 	mkdir -p $DIR/build/linux-mingw32-release
 	cd $DIR/build/linux-mingw32-release
-	echo "cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./cmake/linux-mingw32.cmake -S $DIR -B $DIR/build/linux-mingw32-release"
-	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./cmake/linux-mingw32.cmake $DIR 
+	echo "cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./cmake/linux-mingw32.cmake -DCMAKE_INSTALL_PREFIX=$PWD/_install $DIR"
+	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./cmake/linux-mingw32.cmake -DCMAKE_INSTALL_PREFIX=$PWD/_install $DIR 
 	echo "cmake --build $DIR/build/linux-mingw32-release --target all"
-	cmake --build $DIR/build/linux-mingw32-release --target all
+	make
 fi
 
